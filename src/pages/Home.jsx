98ca0b50058aaa6120c483bb67337ef59d8b2c83
Code25 <chingisenkhbaatar@gmail.com>
2024-03-09 23:38:07 +0100

    imported Island model into homescreen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,49 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../Components/Loader";
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+import Island from "../models/Island";
+{
+  /*
+<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center ">
+  Home
+</div>
+*/
 }
+const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition, rotation];
+  };
+  const [islandScale, islandPostition, islandRotation] =
+    adjustIslandForScreenSize();
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}></Suspense>
+        <directionalLight />
+        <ambientLight />
+        <pointLight />
+        <spotLight />
+        <hemisphereLight />
+        <Island
+          position={islandPostition}
+          scale={islandScale}
+          rotation={islandRotation}
+        />
+      </Canvas>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
