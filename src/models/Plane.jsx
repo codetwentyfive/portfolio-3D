41cdfefad4a68f04d6d036184456f7e3a3cce725
Code25 <chingisenkhbaatar@gmail.com>
2024-03-10 15:32:57 +0100

    plane model now animates when user moves the island model

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import planeScene from "../assets/3d/plane.glb"
-import { useGLTF } from '@react-three/drei'
-const Plane = ({isRotating,...props}) => {
-    const {scene,animations}=useGLTF(planeScene)
-  return (
-    <mesh{...props}>
-        <primitive object={scene}/>     
-    </mesh>
-  )
-}
+import React, { useEffect,useRef } from "react";
+import planeScene from "../assets/3d/plane.glb";
+import { useAnimations, useGLTF } from "@react-three/drei";
+const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef();
+  const { scene, animations } = useGLTF(planeScene);
+  //Use animations from drei helper functions
+  const { actions } = useAnimations(animations, ref);
 
-export default Plane
+  useEffect(() => {
+    if (isRotating) {
+      /*Telling the pplane to use the animation "Take 001" to play
+      when isRotating is active*/
+      actions["Take 001"].play();
+    } else {
+      actions["Take 001"].stop();
+    }
+    //useEffect applies when actions or isRotating is triggered
+  }, [actions, isRotating]);
+  return (
+    <mesh {...props} ref={ref}>
+      <primitive object={scene} />
+    </mesh>
+  );
+};
+
+export default Plane;
