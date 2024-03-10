ececf5ec297ca6dc7cb944daa5db67435e142949
Code25 <chingisenkhbaatar@gmail.com>
2024-03-10 15:31:14 +0100

    animated the sky moving on userinput

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,11 +1,21 @@
 import { useGLTF } from "@react-three/drei";
-import React from "react";
+import React, { useRef } from "react";
+//useframe allows us to do something on every frame
+import { useFrame } from "@react-three/fiber";
 
 import skyScene from "../assets/3d/sky.glb";
-const Sky = () => {
+const Sky = ({ isRotating }) => {
   const sky = useGLTF(skyScene);
+  const skyRef = useRef();
+
+  useFrame((_, delta) => {
+    //animation of the sky scence rotating
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.1 * delta;
+    }
+  });
   return (
-    <mesh>
+    <mesh ref={skyRef}>
       {/*Primitive element*/}
       <primitive object={sky.scene} />
     </mesh>
