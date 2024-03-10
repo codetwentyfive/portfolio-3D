3fbd1649eecd3484d1faaa63e81b0b5581e6bc4d
Code25 <chingisenkhbaatar@gmail.com>
2024-03-10 16:56:01 +0100

    HomeInfo Content

---
+++ src/Components/HomeInfo.jsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
+const InfoBox = ({ text, link, btnText }) => (
+  <div className="info-box">
+    <p className="font-medium sm:text-xl text-center">{text}</p>
+    <Link to={link} className="neo-brutalism-white neo-btn">
+      {btnText}
+      <img src={arrow} className="w-4 h-4 object-contain" />
+    </Link>
+  </div>
+);
+
+const renderContent = {
+  1: (
+    <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+      Hi, I'am <span className="font-semibold">Chingis Enkhbaatar</span> 👋
+      <br />A Full Stack Developer located in <br />
+      Karlsruhe, Germany.
+    </h1>
+  ),
+  2: (
+    <InfoBox
+      text="Worked on many dumplings and picked up many cooking sklls along the way"
+      link="/about"
+      btnText="Learn More"
+    />
+  ),
+  3: (
+    <InfoBox
+      text="Worked on many projects and rediscovered my passion for dumplings!"
+      link="/projects"
+      btnText="Visit my portfolio"
+    />
+  ),
+  4: (
+    <InfoBox
+      text="Let's use our god-given talent of creation and make more dumpling!"
+      link="/contact"
+      btnText="Let's talk"
+    />
+  ),
+};
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null;
+};
+
+export default HomeInfo;
