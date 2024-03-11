00aeb9e2301a32ffd0ad90f15751e094282cd6b4
Code25 <chingisenkhbaatar@gmail.com>
2024-03-11 19:54:36 +0100

    Icon update

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,61 +1,69 @@
-import css from './css.svg'
-import express from './express.svg'
-import git from './git.svg'
-import github from './github.svg'
-import html from './html.svg'
-import javascript from './javascript.svg'
-import mongodb from './mongodb.svg'
-import motion from './motion.svg'
-import mui from './mui.svg'
-import nextjs from './nextjs.svg'
-import nodejs from './nodejs.svg'
-import react from './react.svg'
-import redux from './redux.svg'
-import sass from './sass.svg'
-import tailwindcss from './tailwindcss.svg'
-import typescript from './typescript.svg'
-import linkedin from './linkedin.svg'
-import twitter from './twitter.svg'
-import car from './car.svg'
-import estate from './estate.svg'
-import pricewise from './pricewise.svg'
-import snapgram from './snapgram.svg'
-import summiz from './summiz.svg'
-import threads from './threads.svg'
-import arrow from './arrow.svg'
-import contact from './contact.svg'
-import soundon from './soundon.png'
-import soundoff from './soundoff.png'
-import python from './python.svg'
+import css from "./css.svg";
+import express from "./express.svg";
+import git from "./git.svg";
+import github from "./github.svg";
+import html from "./html.svg";
+import javascript from "./javascript.svg";
+import mongodb from "./mongodb.svg";
+import motion from "./motion.svg";
+import mui from "./mui.svg";
+import nextjs from "./nextjs.svg";
+import nodejs from "./nodejs.svg";
+import react from "./react.svg";
+import redux from "./redux.svg";
+import sass from "./sass.svg";
+import tailwindcss from "./tailwindcss.svg";
+import typescript from "./typescript.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import car from "./car.svg";
+import estate from "./estate.svg";
+import pricewise from "./pricewise.svg";
+import snapgram from "./snapgram.svg";
+import summiz from "./summiz.svg";
+import threads from "./threads.svg";
+import arrow from "./arrow.svg";
+import contact from "./contact.svg";
+import soundon from "./soundon.png";
+import soundoff from "./soundoff.png";
+import python from "./python.svg";
+import caretaker from "./caretaker.png";
+import passau from "./passau.svg";
+import lmu from "./lmu.svg";
+import odin from "./odin.svg";
 
 export {
-    css,
-    express,
-    git,
-    github,
-    html,
-    javascript,
-    mongodb,
-    motion,
-    mui,
-    nextjs,
-    nodejs,
-    react,
-    redux,
-    sass,
-    tailwindcss,
-    typescript,
-    linkedin,
-    twitter,
-    car,
-    estate,
-    pricewise,
-    snapgram,
-    summiz,
-    threads,
-    arrow,
-    contact,
-    soundon,
-    python,
-    soundoff
-}
+  css,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  mongodb,
+  caretaker,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  react,
+  redux,
+  sass,
+  tailwindcss,
+  typescript,
+  linkedin,
+  twitter,
+  car,
+  estate,
+  pricewise,
+  snapgram,
+  summiz,
+  threads,
+  odin,
+  arrow,
+  contact,
+  soundon,
+  lmu,
+  passau,
+  python,
+  soundoff,
+};
