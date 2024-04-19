ab2f6238c7859b882bdc1c9f423b6e97dc232086
code25 <chingisenkhbaatar@gmail.com>
2024-04-19 20:52:54 +0200

    adding the translation wrapper around the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
 import App from "./App";
 import "./index.css";
+import i18n from './i18n'; // Import i18n instance
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+);
