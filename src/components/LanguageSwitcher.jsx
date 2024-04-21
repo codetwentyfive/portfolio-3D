ceac24adc1b99c10f0de74723f7ea2e048ffd85f
code25 <chingisenkhbaatar@gmail.com>
2024-04-21 20:16:58 +0200

    smart update, so the langue button shows the langue you can switch to instead of the one it currently is

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -15,7 +15,7 @@ function LanguageSwitcher() {
     <div className="relative inline-block  bg-white shadow-md rounded-full w-8 h-8  mx-2 ">
       <button className="flex cursor-pointer " onClick={toggleLanguage}>
         <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          {currentLanguage === "de" ? "🇩🇪" : "🇬🇧"}
+          {currentLanguage === "de" ? "🇬🇧" : "🇩🇪"}
         </span>
       </button>
     </div>
