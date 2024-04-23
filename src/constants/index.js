1410862d2998fa38f2cac06bfb0cb0a325ac00e8
code25 <chingisenkhbaatar@gmail.com>
2024-04-23 13:25:15 +0200

    language options for the projects ar now added

--- src/constants/index.js
+++ src/constants/index.js
@@ -160,41 +160,51 @@ export const projects = [
   {
     iconUrl: puzzle,
     theme: "btn-back-pink",
-    name: "Yu-Gi-Oh Memory Game",
-    description:
-      "A game made with react, and python to fetch the card images from the Yu-Gi-Oh! API, currated a collection of the most iconic and also funniest cards!",
+    name: "Yu-Gi-Oh! Card Memory",
+    descriptions: {
+      en: "A game made with react, and python to fetch the card images from the Yu-Gi-Oh! API, currated a collection of the most iconic and also funniest cards!",
+      de: "Ein Spiel programmiert mit React und Python, um die Kartengrafiken aus der Yu-Gi-Oh! API abzurufen. Ich habe eine Sammlung der ikonischsten und lustigsten Karten aus dem Yu-Gi-Oh! Universum für dieses Spiel zusammengestellt!",
+    },
     link: "https://github.com/codetwentyfive/memory-card",
   },
   {
     iconUrl: mountain,
     theme: "btn-back-green",
     name: "Altan Mountain",
-    description:
-      "A mockup of an e-commerce store, with a modern approach towards product displays, which pop out of the screen and are interactive as well as a theme switcher ",
+    descriptions: {
+      en: "A mockup of an e-commerce store, with a modern approach towards product displays, which pop out of the screen and are interactive as well as a theme switcher.",
+      de: "Ein Prototype E-Commerce-Shop mit einem modernen Ansatz zur Produktanzeige, bei dem die Produkte aus dem Bildschirm hervorstechen und interaktiv sind, sowie einem Designwechsler welcher sich dem bevorzugten Beleuchtungsverhältnissen des Benutzers anpasst.",
+    },
     link: "https://github.com/codetwentyfive/shopping-cart",
   },
   {
     iconUrl: weather,
     theme: "btn-back-blue",
     name: "Weather App",
-    description:
-      "Designed and built a weather app, which not only displays the wanted weather information, but also fetches the users current location and automatically displays the current weather, using OpenCage Data API. ",
+    descriptions: {
+      en: "Designed and built a weather app, which not only displays the wanted weather information, but also fetches the users current location and automatically displays the current weather, using OpenCage Data API. ",
+      de: "Eine Wetter-App, die nicht nur die gewünschten Wetterinformationen am gesuchten Ort anzeigt, sondern auch den aktuellen Standort des Benutzers abruft und automatisch das aktuelle Wetter anzeigt, mit der Verwendung der OpenCage Data API. ",
+    },
     link: "https://github.com/codetwentyfive/Weather-App",
   },
   {
     iconUrl: cv,
     theme: "btn-back-yellow",
     name: "CV-App",
-    description:
-      "Developed a web application for making inputing CVs more appealing, instantly see the changes to the CV as you type them.",
+    descriptions: {
+      en: "Developed a web application for making inputing CVs more appealing, instantly see the changes to the CV as you type them.",
+      de: "Eine Webanwendung, um das Eingeben von Lebensläufen ansprechender zu gestalten. Sie sehen sofort die Änderungen am Lebenslauf, während Sie sie eingeben.",
+    },
     link: "https://github.com/codetwentyfive/cv-application",
   },
   {
     iconUrl: sketch,
     theme: "btn-back-red",
     name: "Etch-A-Sketch",
-    description:
-      "Build a canvas modeled after the nostalgic child-toy, with an adjustable canvas size,editing tools,as well as a save function i was very proud of back when i figured it out ",
+    descriptions: {
+      en: "Build a canvas modeled after the nostalgic child-toy, with an adjustable canvas size,editing tools,as well as a save function i was very proud of back when i figured it out ",
+      de:"Eine Leinwand, die dem nostalgischen Kinderspielzeug nachempfunden ist, mit einer verstellbaren Leinwandgröße, diversen Malwerkzeugen sowie einer Speicherfunktion, auf die ich sehr stolz war, als ich sie zum erste Mal zum laufen bekommen habe.",
+    },
     link: "https://github.com/codetwentyfive/Etch-A-Sketch?tab=readme-ov-file",
   },
 ];
