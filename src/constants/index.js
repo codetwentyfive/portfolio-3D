d55a8ca2633ee88869743df2da4a9e2bc0b26283
code25 <chingisenkhbaatar@gmail.com>
2024-06-16 18:42:19 +0200

    feat: added livelinks

--- src/constants/index.js
+++ src/constants/index.js
@@ -240,7 +240,8 @@ export const projects = [
       en: "A game made with react, and python to fetch the card images from the Yu-Gi-Oh! API, currated a collection of the most iconic and also funniest cards!",
       de: "Ein Spiel programmiert mit React und Python, um die Kartengrafiken aus der Yu-Gi-Oh! API abzurufen. Ich habe eine Sammlung der ikonischsten und lustigsten Karten aus dem Yu-Gi-Oh! Universum für dieses Spiel zusammengestellt!",
     },
-    link: "https://github.com/codetwentyfive/memory-card",
+    link: "https://yugioh-memorygame.vercel.app/",
+    github: "https://github.com/codetwentyfive/memory-card",
   },
   {
     iconUrl: mountain,
@@ -250,7 +251,8 @@ export const projects = [
       en: "A mockup of an e-commerce store, with a modern approach towards product displays, which pop out of the screen and are interactive as well as a theme switcher.",
       de: "Ein Prototype E-Commerce-Shop mit einem modernen Ansatz zur Produktanzeige, bei dem die Produkte aus dem Bildschirm hervorstechen und interaktiv sind, sowie einem Designwechsler welcher sich dem bevorzugten Beleuchtungsverhältnissen des Benutzers anpasst.",
     },
-    link: "https://github.com/codetwentyfive/shopping-cart",
+    link: "https://shopping-carti.vercel.app/",
+    github: "https://github.com/codetwentyfive/shopping-cart",
   },
   {
     iconUrl: weather,
@@ -260,7 +262,8 @@ export const projects = [
       en: "Designed and built a weather app, which not only displays the wanted weather information, but also fetches the users current location and automatically displays the current weather, using OpenCage Data API. ",
       de: "Eine Wetter-App, die nicht nur die gewünschten Wetterinformationen am gesuchten Ort anzeigt, sondern auch den aktuellen Standort des Benutzers abruft und automatisch das aktuelle Wetter anzeigt, mit der Verwendung der OpenCage Data API. ",
     },
-    link: "https://github.com/codetwentyfive/Weather-App",
+    link: "https://chingis-weather-app.vercel.app/",
+    github: "https://github.com/codetwentyfive/Weather-App",
   },
   {
     iconUrl: cv,
@@ -270,7 +273,8 @@ export const projects = [
       en: "Developed a web application for making inputing CVs more appealing, instantly see the changes to the CV as you type them.",
       de: "Eine Webanwendung, um das Eingeben von Lebensläufen ansprechender zu gestalten. Sie sehen sofort die Änderungen am Lebenslauf, während Sie sie eingeben.",
     },
-    link: "https://github.com/codetwentyfive/cv-application",
+    link: "https://cv-application-navy-delta.vercel.app/",
+    github: "https://github.com/codetwentyfive/cv-application",
   },
   {
     iconUrl: sketch,
@@ -280,6 +284,7 @@ export const projects = [
       en: "Build a canvas modeled after the nostalgic child-toy, with an adjustable canvas size,editing tools,as well as a save function i was very proud of back when i figured it out ",
       de: "Eine Leinwand, die dem nostalgischen Kinderspielzeug nachempfunden ist, mit einer verstellbaren Leinwandgröße, diversen Malwerkzeugen sowie einer Speicherfunktion, auf die ich sehr stolz war, als ich sie zum erste Mal zum laufen bekommen habe.",
     },
-    link: "https://github.com/codetwentyfive/Etch-A-Sketch?tab=readme-ov-file",
+    link: "https://codetwentyfive.github.io/Etch-A-Sketch/",
+    github: "https://github.com/codetwentyfive/Etch-A-Sketch?tab=readme-ov-file",
   },
 ];
