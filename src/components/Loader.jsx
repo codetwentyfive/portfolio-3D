ff34970e33f3877fcc1fe34a4e1c8f727af7e867
code25 <chingisenkhbaatar@gmail.com>
2025-01-19 17:03:38 +0100

    Enhance Loader component to display loading progress
    - Integrated useProgress hook from @react-three/drei to track loading progress.
    - Updated Loader component to show a percentage of loading completion below the spinner.
    - Improved styling for better alignment and presentation of loading state.

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,24 @@
-import { Html } from "@react-three/drei";
+import { Html, useProgress } from "@react-three/drei";
+
 const Loader = () => {
+  const { progress } = useProgress();
+  
   return (
-    <Html>
-      <div className="flex w-full h-full justify-center items-center ">
-        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin" />
-      </div>
+    <Html
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        transform: "translate(-50%, -50%)"
+      }}
+    >
+      <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin" />
+      <p className="text-sm text-slate-500 mt-4 font-medium">
+        {progress.toFixed(0)}%
+      </p>
     </Html>
   );
 };
