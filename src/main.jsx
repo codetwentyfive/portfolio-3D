21188f259afec2bdf3ca21bdb55986f039e78317
code25 <chingisenkhbaatar@gmail.com>
2025-01-21 21:38:36 +0100

    Rename project to "3d-portfolio" and add @vercel/analytics for enhanced tracking
    - Updated package and lock files to reflect the new project name.
    - Added @vercel/analytics dependency for improved analytics integration.
    - Integrated Analytics component in main.jsx for tracking user interactions.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
+import { Analytics } from '@vercel/analytics/react';
 import App from "./App";
 import "./index.css";
 import i18n from './i18n'; // Import i18n instance
@@ -8,5 +9,6 @@ import i18n from './i18n'; // Import i18n instance
 ReactDOM.createRoot(document.getElementById("root")).render(
     <I18nextProvider i18n={i18n}>
       <App />
+      <Analytics />
     </I18nextProvider>
 );
