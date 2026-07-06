a1343acb766ea126b8cd1a8420ace8db132dfe87
code25 <chingisenkhbaatar@gmail.com>
2026-07-06 11:39:39 +0200

    drop vite leftovers, generate sitemap/robots, refresh CLAUDE.md

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Home from "./Home";
-import About from "./About";
-import Projects from "./Projects";
-import Contact from "./Contact";
-import Impressum from "./Impressum";
-import Privacy from "./Privacy";
-import Legal from "./Legal";
-
-export { Home, About, Projects, Contact, Impressum, Privacy, Legal };
